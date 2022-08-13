e93d95cd52091e51c1052ec29aaa576922f99d95
Luan Carlos <99558903+SkollElla@users.noreply.github.com>
2022-08-13 12:30:40 -0300

    Add files via upload

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,89 @@
+﻿using static Calculadora.Variaveis;
+
+namespace calculadora
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora iniciada.\nAperte Enter para continuar!");
+            Console.ReadKey();
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Digite o primeiro numero.");
+                double num1 = double.Parse(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine("{0}", num1);
+                Console.WriteLine("Agora digite a operação desejada.");
+                string op = Console.ReadLine();
+                Console.Clear();
+                if (op == e[0] || op == e[1] || op == e[2] || op == e[3])
+                {
+                    Console.WriteLine("{0} {1}", num1, op);
+                    Console.WriteLine("Por fim digite o segundo numero.");
+                    double num2 = double.Parse(Console.ReadLine());
+                    Console.Clear();
+
+                    switch (op)
+                    {
+                        case "+":
+                            SetRes(num1 + num2);
+                            Console.WriteLine("{0} {1} {2} = {3}", num1, op, num2, GetRes());
+                            break;
+
+                        case "-":
+                            SetRes(num1 - num2);
+                            Console.WriteLine("{0} {1} {2} = {3}", num1, op, num2, GetRes());
+                            break;
+
+                        case "*":
+                            SetRes(num1 * num2);
+                            Console.WriteLine("{0} {1} {2} = {3}", num1, op, num2, GetRes());
+                            break;
+
+                        case "/":
+                            SetRes(num1 / num2);
+                            Console.WriteLine("{0} {1} {2} = {3}", num1, op, num2, GetRes());
+                            break;
+                        default:
+                            Console.WriteLine("Operação incorreta!");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Operação incorreta!\nAperte Enter para continuar.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                Console.WriteLine("Digite 0 fazer outra operação ou digite 1 para sair!");
+                string v = Console.ReadLine();
+
+                if (v == "0")
+                {
+                    Y = true;
+                    Console.Clear();
+                    Console.WriteLine("Nova operação.");
+                }
+                else if (v == "1" || v == "")
+                {
+                    Y = false;
+                    Console.Clear();
+                    Console.WriteLine("Calculadora encerrada.");
+                }
+                else
+                {
+                    Y = false;
+                    Console.Clear();
+                    Console.WriteLine("Valor não correspondido calculadora encerrada!");
+                }
+            }
+            while (Y);
+            {
+            
+            }
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Variaveis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Variaveis
+    {
+        private static double res;
+
+        public static double GetRes()
+        {
+            return res;
+        }
+
+        public static void SetRes(double value)
+        {
+            res = value;
+        }
+
+        public static bool Y { get; set; }
+        public static string[] e = { "+", "-", "/", "*" };
+    }
+}
